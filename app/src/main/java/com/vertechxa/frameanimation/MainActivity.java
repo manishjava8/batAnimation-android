1334248d7c3fcf39c7fce255d2be6a3953911ec0
Manish <mchaurasia@gotprint.net>
2020-01-30 15:55:10 +0530

    Initial Commit

---
+++ app/src/main/java/com/vertechxa/frameanimation/MainActivity.java
@@ -0,0 +1,49 @@
+package com.vertechxa.frameanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AnimationDrawable batAnimation;
+
+    private ImageView batImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        batImage = findViewById(R.id.batId);
+//        batImage.setBackgroundResource(R.drawable.bat_anim);
+//
+//        batAnimation = (AnimationDrawable) batImage.getBackground();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+//        batAnimation.start();
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Animation startAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadein_animation);
+                batImage.startAnimation(startAnimation);
+                // stop the animation
+//                batAnimation.stop();
+            }
+        }, 50);
+
+
+        return super.onTouchEvent(event);
+
+    }
+}
